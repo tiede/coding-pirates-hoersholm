8963ea05641005a7d5af197a8f1078ef89c52c77
Kim Tiedemann <kim@tiede.dk>
2023-07-07 12:15:32 +0200

    Initial Pybadge checkin

---
+++ python/pybadge/reverse-pong-game/code.py
@@ -0,0 +1,20 @@
+import board
+import displayio
+
+# Passer til Pybadge skærmen
+SCREEN_WIDTH = 160
+SCREEN_HEIGHT = 128
+
+# Lav display
+splash = displayio.Group()
+board.DISPLAY.show(splash)
+
+# Lav baggrund med farven 0x1177bb
+color_bitmap = displayio.Bitmap(SCREEN_WIDTH, SCREEN_HEIGHT, 2)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0x1177bb
+bg_sprite = displayio.TileGrid(color_bitmap, x=0, y=0, pixel_shader=color_palette)
+splash.append(bg_sprite)
+
+while True:
+    pass
