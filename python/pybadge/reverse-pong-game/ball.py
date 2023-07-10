458288086c9e04942b8c73a0c899a2c4c9e63a4d
Kim Tiedemann <kim@tiede.dk>
2023-07-10 09:40:33 +0200

    Bolden kan flytte sig op og ned

--- python/pybadge/reverse-pong-game/ball.py
+++ python/pybadge/reverse-pong-game/ball.py
@@ -1,4 +1,5 @@
 from adafruit_display_shapes.circle import Circle
+from adafruit_pybadger import pybadger
 
 class Ball:
     def __init__(self, diameter, start_x, start_y, skaerm_hoejde, skaerm_bredde):
@@ -18,6 +19,9 @@ class Ball:
         # Skærm højde og bredde kan fortælle os om vi bevæger os ud af skærmen
         self.skaerm_hoejde = skaerm_hoejde
         self.skaerm_bredde = skaerm_bredde
+
+        # PyBadger er en klasse der gør det nemt at håndtere knapper
+        self.badger = pybadger
         
     def update(self):
         print("Inde i ball update")
@@ -27,6 +31,17 @@ class Ball:
             self.x += 1
         else:
             self.x -= 1
+
+        # Få bold til at flytte sig
+        # Check for op knap
+        if self.badger.button.up > 0:
+            # Flyt op
+            self.y -= 1
+        
+        # Check for ned knap
+        if self.badger.button.down > 0:
+            # move down
+            self.y += 1
 
         # Opdater cirkelens position
         self.circle.x = self.x
