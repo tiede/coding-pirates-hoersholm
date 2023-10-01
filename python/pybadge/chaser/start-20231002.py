4c4dbb7697a27a88725397d7584aa07962844af9
Kim Tiedemann <kim@tiede.dk>
2023-10-01 11:46:11 +0200

    Added start 20231002

---
+++ python/pybadge/chaser/start-20231002.py
@@ -0,0 +1,84 @@
+import board
+import displayio
+import terminalio
+import time
+from adafruit_display_text import label
+from adafruit_display_shapes.rect import Rect
+from adafruit_display_shapes.circle import Circle
+from adafruit_pybadger import pybadger
+import random
+
+SCREEN_WIDTH = 160 
+SCREEN_HEIGHT = 128
+
+def intersect():
+    circle_x1 = circle.x
+    circle_x2 = circle.x + diameter
+    circle_y1 = circle.y
+    circle_y2 = circle.y + diameter
+
+    rectangle_x1 = rectangle.x
+    rectangle_x2 = rectangle.x + width
+    rectangle_y1 = rectangle.y
+    rectangle_y2 = rectangle.y + height
+
+    intersect_left = rectangle_x1 < circle_x2
+    intersect_right = rectangle_x2 > circle_x1
+    intersect_top = rectangle_y1 < circle_y2
+    intersect_bottom = rectangle_y2 > circle_y1
+
+    if intersect_left and intersect_right and intersect_top and intersect_bottom:
+        return True
+    
+    return False
+
+splash = displayio.Group()
+board.DISPLAY.show(splash)
+
+height = 20
+width = 20
+rectangle = Rect(0, 0, width, height)
+rectangle.fill = 0x0000FF
+splash.append(rectangle)
+
+radius = 5
+diameter = radius * 2
+circle = Circle(80, 64, radius)
+circle.fill = 0xFFFF00
+splash.append(circle)
+
+score = 0
+text_score = label.Label(terminalio.FONT, text=str(score), color=0xFFFFFF, x=0, y=5)
+splash.append(text_score)
+
+ticks = 0
+while True:
+
+    if ticks == 500:
+        rectangle.x = random.randint(0, SCREEN_WIDTH - width)
+        rectangle.y = random.randint(0, SCREEN_HEIGHT - height)
+        ticks = 0
+
+    if pybadger.button.down:
+        if circle.y < SCREEN_HEIGHT - diameter:
+            circle.y = circle.y + 1
+    if pybadger.button.up:
+        if circle.y > 0:
+            circle.y = circle.y - 1
+    if pybadger.button.left:
+        if circle.x > 0:
+            circle.x = circle.x - 1
+    if pybadger.button.right:
+        if circle.x < (SCREEN_WIDTH-diameter):
+            circle.x = circle.x + 1
+
+    if intersect():
+        score = score + 1
+        rectangle.x = -100
+        rectangle.y = -100
+        ticks = 250
+
+    text_score.text = str(score)
+
+    time.sleep(0.001)
+    ticks = ticks + 1
