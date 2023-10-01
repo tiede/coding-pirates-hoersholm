d495edbcc35ee04ac9951defe427829c98e73939
Kim Tiedemann <kim@tiede.dk>
2023-10-01 11:37:40 +0200

    Added more introduktion

--- python/introduktion/turtle/first.py
+++ python/introduktion/turtle/first.py
@@ -1,3 +1,5 @@
 import turtle
 my_pen = turtle.Turtle()
 my_pen.forward(150)
+
+read = input("Press enter to continue")
---
+++ python/introduktion/turtle/square.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+color('red', 'yellow')
+begin_fill()
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+end_fill()
+
+read = input("Press enter to continue")
