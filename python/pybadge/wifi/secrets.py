f79565456e3fee6f8d36086bcfc2a8c28c6668bc
Kim Tiedemann <kim@tiede.dk>
2024-06-10 09:18:51 +0200

    Added wifi code for pybadge

---
+++ python/pybadge/wifi/secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : '2DO',
+    'password' : '2DO',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'github_token' : 'N/A',
+    'hackaday_token' : 'N/A',
+    }
